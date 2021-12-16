44539d75de5cc732640b4c23b066093a9374312a
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2021-12-16 15:48:09 +0100

    Create base components

---
+++ src/screens/MovieBillboard/MovieBillboard.tsx
@@ -0,0 +1,12 @@
+import Movie from '../../components/Movie/Movie';
+
+const MovieBillboard = () => {
+  return (
+    <div>
+      Movie Billboard
+      <Movie />
+    </div>
+  );
+};
+
+export default MovieBillboard;
