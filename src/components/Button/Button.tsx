726126f217f3a50da99a3f2cc02c6dbc94981d0d
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2021-12-17 09:54:20 +0100

    Set Up button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,46 @@
-const Button = () => {
-  return <button>Button</button>;
+import { FunctionComponent } from 'react';
+
+type ButtonType = 'primary' | 'danger';
+
+interface IButton {
+  text?: string;
+  type: ButtonType;
+  clickHandler: () => void;
+}
+
+interface CSSStyle {
+  [key: string]: string | number;
+}
+
+const Button: FunctionComponent<IButton> = ({
+  text = 'Button',
+  type,
+  clickHandler
+}) => {
+  return (
+    <button
+      style={{ ...buttonStyling, ...buttonType[type] }}
+      onClick={clickHandler}
+    >
+      {text.toUpperCase()}
+    </button>
+  );
+};
+
+const buttonStyling: CSSStyle = {
+  border: 'none',
+  background: 'red',
+  cursor: 'pointer',
+  color: 'white',
+  padding: '0.6rem',
+  borderRadius: '0.3rem',
+  fontSize: '0.6rem',
+  margin: '0.2rem'
+};
+
+const buttonType: { [key in ButtonType]: CSSStyle } = {
+  primary: { background: 'red' },
+  danger: { background: 'blue' }
 };
 
 export default Button;
