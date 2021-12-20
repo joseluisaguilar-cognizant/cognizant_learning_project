2de0743f9213b1ed8ff75ad65c6d49c258d87e49
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2021-12-20 15:52:42 +0100

    Utils: object to create regexp

--- src/shared/utils/generateRegexp.ts
+++ src/shared/utils/generateRegexp.ts
@@ -10,13 +10,14 @@ interface GenerateRegexp {
 
 export const generateRegexp: GenerateRegexp = {
   email:
-    '^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$',
+    '^(([^<>()[\\]\\.,;:\\s@"]+(\\.[^<>()[\\]\\.,;:s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
   generateMaxMinLengthRegexp: (min, max) => {
+    // ? Just to check if this function is called more than once (no)
     console.log('regex');
     return `^[a-zA-Z]{${String(min)},${String(max)}}$`;
   },
   generatePassRegexp: (min, max, specialCharacters) =>
-    `^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[${specialCharacters}]).{${String(
+    `^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[${specialCharacters}]).{${String(
       min
     )},${String(max)}}$`
 };
