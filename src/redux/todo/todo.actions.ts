5bc86457d540ebb2091ec78cd67961b44f847ae4
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2021-12-21 16:53:39 +0100

    create todo Reducer

--- src/redux/todo/todo.actions.ts
+++ src/redux/todo/todo.actions.ts
@@ -1,16 +1,22 @@
+import { Task } from '../../interfaces/task.interface';
 import { TodoActionTypes } from './todo.types';
 
-export const createNewTask = (newTask: any) => ({
+export const createNewTask = (newTask: Task) => ({
   type: TodoActionTypes.CREATE_NEW_TASK,
   payload: newTask
 });
 
-export const toggleStatus = (id: number) => ({
-  type: TodoActionTypes.TOGGLE_STATUS,
-  payload: id
+export const deleteTask = (taskId: string) => ({
+  type: TodoActionTypes.REMOVE_TASK,
+  payload: taskId
 });
 
-export const toggleImportance = (id: number) => ({
+export const toggleStatus = (taskId: string) => ({
+  type: TodoActionTypes.TOGGLE_STATUS,
+  payload: taskId
+});
+
+export const toggleImportance = (taskId: string) => ({
   type: TodoActionTypes.TOGGLE_IMPORTANCE,
-  payload: id
+  payload: taskId
 });
--- src/redux/todo/todo.reducer.ts
+++ src/redux/todo/todo.reducer.ts
@@ -4,25 +4,25 @@ import { TodoActionTypes } from './todo.types';
 
 const INITIAL_STATE: Task[] = [
   {
-    id: 1,
+    id: '1',
     isImportant: true,
     isActive: true,
     text: 'Do something'
   },
   {
-    id: 2,
+    id: '2',
     isImportant: true,
     isActive: false,
     text: 'Do something more'
   },
   {
-    id: 3,
+    id: '3',
     isImportant: false,
     isActive: false,
     text: 'Do another thing'
   },
   {
-    id: 4,
+    id: '4',
     isImportant: false,
     isActive: true,
     text: 'Rest'
@@ -32,10 +32,10 @@ const INITIAL_STATE: Task[] = [
 const todoReducer = (state: Task[] = INITIAL_STATE, action: TodoAction) => {
   switch (action.type) {
     case TodoActionTypes.CREATE_NEW_TASK:
-      console.log('task');
-      return state;
+      return [...state, action.payload];
     case TodoActionTypes.REMOVE_TASK:
-      return state;
+      console.log('task id', action.payload);
+      return state.filter((task: Task) => task.id !== action.payload);
     case TodoActionTypes.TOGGLE_IMPORTANCE:
       return state.map((task: Task) =>
         task.id === action.payload
